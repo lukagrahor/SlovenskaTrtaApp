62e3042a84d1069818804a335479f3190bc52d87
lukagrahor <41000453+lukagrahor@users.noreply.github.com>
2022-12-29 13:24:57 +0100

    Dodan Get trte

--- app/src/main/java/com/example/slovenskatrtaapp/MainActivity.java
+++ app/src/main/java/com/example/slovenskatrtaapp/MainActivity.java
@@ -4,11 +4,77 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    private RequestQueue requestQueue;
+    private TextView trte;
+    private String url = "https://slovenskatrta-is.azurewebsites.net/api/v1/Trte";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        requestQueue = Volley.newRequestQueue(getApplicationContext());
+        trte = (TextView) findViewById(R.id.trte);
     }
+
+    public void prikaziTrte(View view) {
+        if (view != null) {
+            JsonArrayRequest request = new JsonArrayRequest(url, jsonArrayListener, errorListener);
+            requestQueue.add(request);
+        }
+    }
+
+    private Response.Listener<JSONArray> jsonArrayListener = new Response.Listener<JSONArray>() {
+        @Override
+        public void onResponse(JSONArray response) {
+            ArrayList<String> data = new ArrayList<>();
+
+            for (int i = 0; i < response.length(); i++) {
+                try {
+                    JSONObject object = response.getJSONObject(i);
+                    String id = object.getString("trteId");
+                    String sorta = object.getString("sorta");
+
+                    data.add(id + " " + sorta);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    return;
+
+                }
+
+                trte.setText("");
+
+                for (String row : data) {
+                    String currentText = trte.getText().toString();
+                    trte.setText(currentText + "\n\n" + row);
+                }
+
+            }
+        }
+    };
+
+    private Response.ErrorListener errorListener = new Response.ErrorListener() {
+        @Override
+        public void onErrorResponse(VolleyError error) {
+            Log.d("REST error", error.getMessage());
+        }
+    };
 }
